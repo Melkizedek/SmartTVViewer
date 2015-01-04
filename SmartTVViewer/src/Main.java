7288ad5483f16f712971f351487fd1f99f4597d4
Melkizedek <martin_reif@yahoo.com>
2015-01-04 05:37:50 +0100

    test

--- SmartTVViewer/src/Main.java
+++ SmartTVViewer/src/Main.java
@@ -2,9 +2,7 @@
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("testitest");
-	System.out.println("2222222222222222");
-	System.out.println("33333333333333");
+	
     }
 
 }
